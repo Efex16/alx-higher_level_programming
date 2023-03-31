e7d9a8c4a9b1ef5083824395225d187df4444af4
Efex16 <111120611+Efex16@users.noreply.github.com>
2023-03-31 22:01:56 +0100

    Update 0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""
-Script that fetches https://intranet.hbtn.io/status
-"""
-import urllib.request
-
-
-def main():
-    """
-    Funtion to print a response of a specific url
-    """
-    url = 'https://intranet.hbtn.io/status'
-    with urllib.request.urlopen(url) as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html)))
-        print('\t- content: {}'.format(html))
-        print('\t- utf8 content: {}'.format(html.decode('utf8')))
+"""  fetches https://alx-intranet.hbtn.io/status """
 
 if __name__ == "__main__":
-    main()
+    from urllib.request import urlopen, Request
+
+    url = "https://alx-intranet.hbtn.io/status"
+    req = Request(url)
+    with urlopen(req) as res:
+        content = res.read()
+
+    print("Body response:")
+    print(f"\t- type: {type(content)}")
+    print(f"\t- content: {content}")
+    print(f"\t- utf8 content: {content.decode('utf-8')}")
